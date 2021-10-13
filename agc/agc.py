64269d6b2e875ece4434e17a3f10ae3d6f99ba07
m2bi Abdelmajid OMARJEE <abdelomarjee@hotmail.com>
2021-10-13 11:41:50 +0200

    fr

--- agc/agc.py
+++ agc/agc.py
@@ -71,25 +71,29 @@ def get_arguments():
 
 def read_fasta(amplicon_file, minseqlen):
     with gzip.open(amplicon_file, "rt") as  monfich:
-    for line in monfich:
-        if line.startswith(">"):
-            if len(seq) > minseqlen:
-                yield seq
-            seq = ""
-        else:
-            seq += line[:-1]
+        seq = ""
+        for line in monfich:
+            if line.startswith(">"):
+                if len(seq) > minseqlen:
+                    yield seq
+                seq = ""
+            else:
+                seq += line[:-1]
+        yield seq
 
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
     generator_seq = read_fasta(amplicon_file, minseqlen)
+    dict_seq = {}
     for seq in generator_seq:
         if seq not in dict_seq.keys():
             dict_seq[seq] = 1
         else:
             dict_seq[seq] += 1
     dict_seq = sorted(dict_seq.items(), key=lambda x: x[1], reverse=True)
-    for elem in dict_seq():
-        yield list(elem)
+    for elem in dict_seq:
+        if elem[1]> mincount:
+            yield list(elem)
 
 
 def get_unique(ids):
@@ -128,15 +132,106 @@ def get_identity(alignment_list):
 def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
     pass
 
+
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+    generator = list(chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size))
+    list_otu = [generator[0]]
+    nb_otu = 1
+    for sequence1 in generator[1:]:
+        index = 0
+        flag = True
+        print("a")
+        while index < nb_otu :
+            align = nw.global_align(sequence1[0], list_otu[index][0], gap_open=-1, gap_extend=-1,\
+            matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),"MATCH")))
+            print(get_identity(align))
+            if get_identity(align) > 97.0:
+                flag = False
+                break
+            index += 1
+        if flag:
+            list_otu.append(list(sequence1))
+            nb_otu +=1
+    return list_otu
 
 def fill(text, width=80):
-    """Split text with a line return to respect fasta format"""
+    """Sp, lit text with a line return to respect fasta format"""
     return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
 
 def write_OTU(OTU_list, output_file):
-    pass
+    with open(output_file, "w") as filout:
+        for index, otu in enumerate(OTU_list):
+            filout.write(f">OTU_{index + 1} occurrence:{otu[1]}\n")
+            filout.write(fill(otu[0]) + '\n')
+
+
+def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+    generator_kmer = cut_kmer(sequence, kmer_size)
+    for kmer in generator_kmer:
+        if kmer not in kmer_dict.keys():
+            kmer_dict[kmer] = [id_seq]
+        elif id_seq not in kmer_dict[kmer]:
+            kmer_dict[kmer].append(id_seq)
+    return kmer_dict
+
+
+def search_mates(kmer_dict, sequence, kmer_size):
+    generator_kmer = cut_kmer(sequence, kmer_size)
+    list_tmp = []
+    for k_mer in generator_kmer:
+        if k_mer in kmer_dict.keys():
+            list_tmp = list_tmp + kmer_dict[k_mer]
+    best_mates = Counter(list_tmp).most_common(2)
+    return [best_mates[0][0], best_mates[1][0]]
+
+
+def detect_chimera(perc_identity_matix):
+    mean_std = statistics.mean([statistics.stdev(seg) for seg in perc_identity_matix])
+    if mean_std < 5.0:
+        return False
+    flag = 0
+    if perc_identity_matix[0][0] > perc_identity_matix[0][1]:
+        flag = 1
+    for seg in perc_identity_matix:
+        if flag == 0 and seg[0] > seg[1]:
+            return True
+        if flag == 1 and seg[0] < seg[1]:
+            return True
+    return False
+
+def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+    generator = dereplication_fulllength(amplicon_file, minseqlen, mincount)
+    i = 0
+    not_chimeral = []
+    k_mer_dict = {}
+    for seq in generator:
+        if i < 2:
+            k_mer_dict = get_unique_kmer(k_mer_dict, seq[0], i, kmer_size)
+            i += 1
+            print(i)
+            not_chimeral.append(seq[0])
+            yield seq
+        else:
+            best = search_mates(k_mer_dict, seq[0], kmer_size)
+            chunk_list = get_chunks(seq[0], chunk_size)
+            chunk_seq_list = [get_chunks(not_chimeral[best[0]], chunk_size)]
+            chunk_seq_list += [get_chunks(not_chimeral[best[1]], chunk_size)]
+            perc_identity_matrix = [[] for c in range(len(chunk_list))]
+            for j in range(len(chunk_seq_list)):
+                for l,chunk in enumerate(chunk_list):
+                    perc_identity_matrix[l].append(get_identity(
+                                nw.global_align(chunk, chunk_seq_list[j][l], 
+                                    gap_open=-1, gap_extend=-1, matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                        '../agc')) + "/MATCH")))
+            if detect_chimera(perc_identity_matrix):
+                k_mer_dict = get_unique_kmer(k_mer_dict, seq[0], i, kmer_size)
+                i += 1
+                not_chimeral.append(seq[0])
+                yield seq
+
+
+
+
 
 #==============================================================
 # Main program
@@ -148,7 +243,9 @@ def main():
     # Get arguments
     args = get_arguments()
     # Votre programme ici
-
+    if args.output_file:
+        OTU_list = abundance_greedy_clustering(args.amplicon_file, args.minseqlen, args.mincount, args.chunk_size, args.kmer_size)
+        write_OTU(OTU_list, args.output_file)
 
 if __name__ == '__main__':
     main()
